ed9f0b71cdf6009b8fc73105d67b356ff36e621f
mk19121912 <153469516+mk19121912@users.noreply.github.com>
2023-12-10 19:41:55 -0500

    Add files via upload

---
+++ memes.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+import requests
+import json
+
+def get_meme():
+    url= "https://meme-api.com/gimme"
+    response = json.loads(requests.request("GET", url).text)
+    print(response)
+    meme_large = response["preview"][-2]
+    subreddit = response["subreddit"]
+    return meme_large, subreddit
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    meme_pic, subreddit = get_meme()
+    return render_template("meme_index.html", meme_pic=meme_pic, subreddit=subreddit)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
